904f3d2d413ccddc6606e3d41fc010e085bc500c
Alexander Mayorov <alexander@majorov.su>
2018-08-21 15:05:54 +0300

    Update getf.js

--- getf.js
+++ getf.js
@@ -1,4 +1,6 @@
-
+/**
+ * Get string path by input object
+ */
 const path = (p = {}, k) =>
     (
         (k = Object.getOwnPropertyNames(p)[0]),
@@ -6,33 +8,35 @@ const path = (p = {}, k) =>
                 ? `${k}/${path(p[k])}`
                 : k
     );
-
+/**
+ * Get property from object by filed name or string path
+ */
 const getf = (...data) =>
     (
         (getf.$def$ = void null)
-            ,
+        ,
         (data[0].raw != void 0) && (data[0] = '' + data[0])
-            ,
+        ,
         ('object' == typeof data[0]) && (data[0] = path(data[0]))
-            ,
-            getf.$data$ =
-                (typeof data[0] == 'string') && (0 < data.length && data.length < 3)
-                    ? (
-                    (data.length == 2 && 'string' == typeof data[1])
-                    && (getf.$def$ = data[1])
-                        ,
-                        getf.$data$ = data[0].trim().split(/[\s\W]+/g)
-                )
-                    : data
+        ,
+        getf.$data$ =
+            (typeof data[0] == 'string') && (0 < data.length && data.length < 3)
+               ? (
+                  (data.length == 2 && 'string' == typeof data[1])
+                   && (getf.$def$ = data[1])
+                   ,
+                   getf.$data$ = data[0].trim().split(/[\s\W]+/g)
+                 )
+              : data
             ,
             (obj = {}, def = void null) =>
                 getf.$data$.reduce((a, b) =>
-                        a != void Object && void NaN != a[b]
-                            ? a[b]
-                            : (def ? def : getf.$def$)
-                    ,
-                    obj
-                )
+                   a != void Object && void NaN != a[b]
+                     ? a[b]
+                     : (def ? def : getf.$def$)
+                   ,
+                   obj
+            )
     );
 
 getf.path = path;
